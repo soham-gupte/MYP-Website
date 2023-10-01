5f0a6e0fa3112b310c4c1962a3a5c17cf81e8aca
Soham Gupte <soham88gupte@gmail.com>
2023-10-01 17:26:31 +0530

    Know Your right

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import './App.css';
 import { AboutUs } from './components/AboutUs';
 import { Navbar } from './components/Navbar';
 import { HeroSection } from './components/HeroSection';
+import { KnowYourRight } from './components/KnowYourRight';
 function App() {
   return (
     <div className="App">
       <Navbar/>
-      <HeroSection/>
-      <AboutUs/>
+      {/* <HeroSection/> */}
+      {/* <AboutUs/> */}
+      <KnowYourRight/>
     </div>
   );
 }
---
+++ src/components/KnowYourRight.js
@@ -0,0 +1,94 @@
+import "./KnowYourRightStyles.css";
+
+export function KnowYourRight() {
+
+    return (
+        <div className="know-your-right">
+            <h1>Know Your Right</h1>
+            <div className="kyr-content-grid">
+                <div className="kyr-content">
+                    <h2>Criteria of Voting</h2>
+                    <p>1. Citizen of India having attained 18 years of age.</p><br></br>
+                    <p>2. His/her name must appear in the Voting List of Assembly/ Parliament elections, as the case may be.</p><br></br>
+                    <p>3. He/she must have a Voters Identity Card. In case Voter I-Card is not there, he/she can show a proof of identity acceptable to the returning officer.</p><br></br><hr></hr><br></br>
+                    <p><b>What Happens if You Don't Vote?</b></p><br></br><br></br>
+                    <p>If you don't vote, you have not shown preference for anyone standing in the election, or any side in a referendum campaign. Your vote will not be counted for either side or directly affect the result.</p><br></br>
+                    <p>You have not accessed the Statutory right given to you by the constitution for the better growth of the country!</p>
+                </div>
+                <div className="kyr-content">
+                    <h2>Article 326</h2>
+                    <p>The elections to the House of the People and to the Legislative Assembly of every State shall be on the basis of adult suffrage; that is to say, every person who is a citizen of India and who is not less than 18 years of age on such date as may be fixed in that behalf by or under any law made by the appropriate Legislature. </p><br></br><hr></hr><br></br>
+                    <p><b>If the Candidate is Not of Your Preference
+                        We suggest 'NOTA Voting' rather than abstaining if you do not agree with any of the options on the ballot paper!</b></p>
+                </div>
+            </div>
+            <div className="kyr-faq">
+                <h2>FAQs</h2><br></br><hr></hr><br></br><br></br>
+                <p><b>Q. How to make a voter id?</b></p><br></br>
+                <p>I. Before applying for a Voter ID Card online, make sure you have the following documents ready (and scanned/soft copies):<br />
+                    <p>A) Valid Phone Number (other than landline number)</p>
+                    <p>B) Address proof - (passport/driving licence/ration card/electricity bill/telephone bill/gas bill in jpg format)</p>
+                    <p>C) Age proof - driving licence/passport /pan card/aadhaar /birth certificate.</p>
+                    <p>D) Photo (personal photo) (jpg format)</p>
+                    <i>Any one of the above documents can be used as a government identification.</i><br></br>
+                </p><br />
+                <p>II. Log on to <a href="https://markyourpresence.org.in/">https://markyourpresence.org.in/</a> which will redirect you to Nation Voters Services Portal</p><br />
+                <p>III. Click on register for new voters id (the form 6 will appear, which needs to be filled completely)</p><br />
+                <p>IV. After the form 6 is filled and all the information required is uploaded, one should submit the
+                    form</p><br />
+                <p>V. You will receive a message about the enrollment number which you can use to check the status of your voters identity card</p><br />
+                <p>VI. You can track your status on the home page of our site by clicking on the ‘track status’ button</p><br />
+                <p>VII. You can also find out your nearest elections office , polling Booth and even check if your name is on the electoral list by clicking on the ‘our state’ tab
+                    8.log on to our website and become a voter and Mark your presence this election.</p><br /><br /><br />
+                <p><b>Q. What is the minimum age for enrollment in an electoral roll?</b></p><br/>
+                <p>A. Eighteen. The minimum age for registration of a voter is 18 years.</p>
+                <p>Reference: For every constituency, there is a voters list, Article 326 of the Constitution.</p><br /><br /><br />
+                <p><b>Q. Was 18 the minimum voting age in India from the beginning?</b></p><br/>
+                <p>A. No. Earlier, the age for registration of a voter was 21 years. The minimum age of registration of a voter has been brought down to 18 years. This has been made effective from 28th March, 1989.</p>
+                <p>Reference: Through the 61st amendment Act, 1988 of the Constitution read with Act 21 of 1989 amending the R. P. Act, 1950.</p><br /><br /><br />
+                <p><b>Q. Which is the relevant date for determining the age qualification of 18 years? Suppose, you have completed 18 years of age today. Can you get yourself registered as a voter?</b></p><br/>
+                <p>A. The qualifying date means the first day of January of the year in which the electoral roll is prepared or revised.</p>
+                <p>Reference: According to Section 14 (b) of the R. P. Act, 1950.</p><br/><br/><br/>
+                <p><b>Q. Can a non-citizen of India become a voter?</b></p><br/>
+                <p>A. No, a person who is not a citizen of India cannot be registered as a voter.</p><br/><br/><br/>
+                <p><b>Q. Can a non-resident Indian citizen become a voter?</b></p><br/>
+                <p>A. No, only a person who is ordinarily resident in a constituency is entitled to be registered in the electoral roll of that constituency. However, such non-resident Indian Citizens who are employed under Govt. of India in a post outside India are eligible to be registered as voters.</p><br/><br/><br/>
+                <p><b>Q. If I am working and living in Delhi, can I be a voter in my native village?</b></p><br/>
+                <p>A. No, You can be enrolled at Delhi only and not in your native village. If you are working in Delhi and residing there, you are an ordinary resident of Delhi in terms of Sec 19 (b).</p><br/><br/><br/>
+                <p><b>Q. Can one be enrolled at more than one place?</b></p><br/>
+                <p>A. No, a person cannot be enrolled as a voter in more than one place in the same constituency or in more than one constituency.</p><br/><br/><br/>
+                <p><b>Q. How can I get registered/enrolled in the Electoral Roll?</b></p><br/>
+                <p>A. You have to submit a filled in Form - 6.</p><br/><br/><br/>
+                <p><b>Q. What is the procedure to make corrections in such names / other details that have been misspelt in the Electoral Roll?</b></p><br/>
+                <p>A. For incorporation of corrections in the Electoral Rolls, You have to submit Form – 8</p><br/><br></br>
+                <p>Following are the various forms useful for registration as voter, corrections, change in address etc.</p><br></br>
+                <p>For inclusion of names - Form 6</p>
+                <p>For any objection on inclusion of names - Form 7</p>
+                <p>For correction of entries in the Electoral Rolls - Form 8</p>
+                <p>For transposition of entry in electoral roll - Form 8A</p><br/><br/><br/>
+                <p><b>Q. I have shifted my residence recently. How do I ensure that I am enrolled in my new place of residence and my name is deleted from the old place?</b></p><br/>
+                <p>A. In case the new residence is in the same constituency please fill Form 8A otherwise form 6 and submit to the ERO or the AERO of the area of your new residence.</p><br/><br/><br/>
+                <p><b>Q. I have shifted my residence recently. I have Photo EPIC Card with the old address. Can I get a new EPIC Card for the present address?</b></p><br/>
+                <p>A. First of all please ensure that you have enrolled yourself in the Electoral Roll of the concerned AC, where you are now residing following the procedure given in Ans. No.10. Subsequently, a new card at the present address will be issued to you.</p><br/><br/><br/>
+                <p><b>Q. My old EPIC Card is defective. I would like to have a new EPIC Card with correct particulars. What is the procedure?</b></p><br/>
+                <p>A. You can get your I Card rectified by depositing it in the Office of the ERO concerned or at the Photography Centre when the work of preparation of Photo ID card begins.</p><br/><br/><br/>
+                <p><b>Q. I do not have a ration card. Can I get enrolled without a ration card? What are the other documents, which I can show as proof of my residence?</b></p><br/>
+                <p>A. Ration Card is not necessary, however, you can show any other proof of residence like Passport, Bank Pass Book, Driving licence etc. or any Govt. document to facilitate the work of registration.</p><br/><br/><br/>
+                <p><b>Q. I am a tenant and my landlord does not want me to get enrolled. How can I get enrolled as a voter?</b></p><br/>
+                <p>A. To get enrolled in the voter list is your statutory right. Please check the Electoral roll of your area ERO (SDM)/AERO office. If your name is not included please fill up Form 6 and deposit it with the ERO.</p><br/><br/><br/>
+                <p><b>Q. I have attained / will be attaining the age of 18. What proof do I need to show to get enrolled?</b></p><br/>
+                <p>A. You can submit the proof of Date of Birth from the authorised agency (Passport, passing certificate, Date of Birth certificate etc.)</p><br/><br/><br/>
+                <p><b>Q. When I fill up Form 4, should I attach proof of my residence with Form 4?</b></p><br/>
+                <p>A. It is not essential but if you attach a proof of residence, it will help early verification of the particulars given by you.</p><br/><br/><br/>
+                <p><b>Q. An Enumerator has visited my house and taken down the details. How do I ensure that my name is finally included in the electoral rolls?</b></p><br/>
+                <p>A. He must have handed over to you the record of Enumeration, which is your acknowledgement. You can check your name in the draft roll, which will be published and available at the office of the ERO concerned or one can go to the election commission official website and enter his epic number and find out.</p><br/><br/><br/>
+                <p><b>Q. I have lost my old EPIC Card. How can I get a new EPIC Card?</b></p><br/>
+                <p>A. You can deposit a copy of the FIR lodged at the Police Station. You will get a new EPIC Card after a deposit of Rs. 25. The dates for issuing EPIC Cards are published in leading Newspapers.</p><br/><br/><br/>
+                <p><b>Q. Who is responsible for the preparation of electoral rolls for a Parliamentary or Assembly Constituency?</b></p><br/>
+                <p>A. The Electoral Registration Officer (ERO). In the case of Delhi, these are the area Sub-Divisional Magistrates/Additional District Magi-states. The Electoral Registration officer is responsible for the preparation of electoral rolls for an Assembly Constituency which itself is the roll for the Parliamentary Constituency in so far as that Assembly Segment is concerned.</p><br/><br/><br/>
+                <p><b>Q. Who appoints the Electoral Registration officer?</b></p><br/>
+                <p>A. The Election Commission of India, in consultation with the State/UT Government, appoints an Officer of the Government or the Local Authorities as the Electoral Registration Officer. In addition, the Election Commission of India also appoints one or more Assistant Electoral Registration Officers to assist the Electoral Registration Officer in the performance of his functions in the matter of preparation/revision of electoral rolls.</p><br/><br/><br/>
+            </div>
+        </div>
+    )
+}
