85701dbdb50dc8528e0f42fc842b0c977c4bdb9f
Soham Gupte <soham88gupte@gmail.com>
2023-12-27 00:21:14 +0530

    some content still left to add

--- src/components/OurProjects.js
+++ src/components/OurProjects.js
@@ -11,15 +11,20 @@ function ProjectGrid() {
         <Row xs={1} md={3} className="g-4">
             {ProjectsData.map((item, index) => (
                 <Col key={index}>
-                    <Card className='our-proj-card'>
-                        <Card.Img variant="top" src= {haha}/>
-                        <Card.Body>
+                    <Card className='our-proj-card' bg='dark' text='light'>
+                        <Card.Img src= {haha} style={{ width: '100%' }}/>
+                        <Card.Body className="custom-card-body">
+                    <hr className="card-divider" />
                             <Card.Title>
                                 <h2>{item.title}</h2>
                             </Card.Title>
+                            <hr className="card-divider" />
                             <Card.Text>
                                 <p>{item.desc}</p>
                             </Card.Text>
+                            <a href={item.link} className="custom-link">
+                                Read more
+                            </a>
                         </Card.Body>
                     </Card>
                 </Col>
